3747f6f3d9464df2a0abce404c15b17f24b6478d
Nick Vigier <nvigier46@gmail.com>
2024-05-29 10:28:56 +0100

    test new deployment

--- src/server-actions/index.ts
+++ src/server-actions/index.ts
@@ -1,5 +1,5 @@
 export async function _getUser(){
-  console.log("Starting async function 2");
+  console.log("Starting async function 2!");
   await new Promise(resolve => setTimeout(resolve, 500));
   console.log("Completed async function 2");
   return {ok:true};
